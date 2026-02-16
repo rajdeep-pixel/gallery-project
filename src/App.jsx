5d19d5ff51cce2fc4b0546b7fa3cf96ad3458282
Rajdeep Shaw <rs4894@srmist.edu.in>
2026-02-17 03:00:22 +0530

    Basic Pagination and other UI changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,79 @@
-import React, { use, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import axios from "axios";
 
 const App = () => {
   const [userData, setUserData] = useState([]);
 
+  const [index, setIndex] = useState(1);
 
   const getData = async () => {
     const response = await axios.get(
-      "https://picsum.photos/v2/list?page=4&limit=30")
-    
-      setUserData(response.data)
-    
-  }
-  let printUserData = "No user avaliable"
+      `https://picsum.photos/v2/list?page=${index}&limit=10`,
+    );
 
-  if (printUserData.length > 0) {
-    printUserData = userData.map(function (elem,idx) {
+    setUserData(response.data);
+  };
 
-      return <div key={idx}>
-        <a href={elem.url} target="_blank">
-          <div className="h-60 w-60 overflow-hidden bg-white rounded-xl">
-        <img className="h-full w-full object-cover" src={elem.download_url} alt="" />
-      </div>
-      <h2 className="font-bold text-lg">{elem.author}</h2>
-        </a>
-      </div>
+  useEffect(
+    function () {
+      getData();
+    },
+    [index],
+  );
+
+  let printUserData = (
+    <h3 className="text-white text-xs absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      LOADING ...
+    </h3>
+  );
+
+  if (userData.length > 0) {
+    printUserData = userData.map(function (elem, idx) {
+      return (
+        <div key={idx}>
+          <a href={elem.url} target="_blank">
+            <div className="h-60 w-60 overflow-hidden bg-white rounded-xl">
+              <img
+                className="h-full w-full object-cover"
+                src={elem.download_url}
+                alt=""
+              />
+            </div>
+            <h2 className="font-bold text-lg">{elem.author}</h2>
+          </a>
+        </div>
+      );
     });
   }
   return (
-      <div className="bg-black overflow-auto text-white h-screen">
+    <div className="bg-black overflow-auto p-4 text-white h-screen">
+      <div className="flex flex-wrap gap-4 p-2 h-[82%]">{printUserData}</div>
+
+      <div className="flex justify-center gap-6 items-center p-4">
         <button
-          onClick={getData}
-          className="bg-green-600 active:scale-95 text-white m-3 px-5 py-2 rounded"
+          className="bg-amber-400 text-black text-sm cursor-pointer active:scale-95 rounded px-4 py-2 font-semibold "
+          onClick={() => {
+            if (index > 1) {
+              setIndex(index - 1);
+              setUserData([]);
+            }
+            
+          }}
         >
-          Get Data
+          Previous
         </button>
-        <div className="flex flex-wrap gap-4 text-white">{printUserData}</div> 
+        <button
+          className="bg-amber-400 text-black text-sm cursor-pointer active:scale-95 rounded px-4 py-2 font-semibold 
+          "
+          onClick={() => {
+            setIndex(index + 1);
+            setUserData([]);
+          }}
+        >
+          Next
+        </button>
       </div>
+    </div>
   );
 };
 
